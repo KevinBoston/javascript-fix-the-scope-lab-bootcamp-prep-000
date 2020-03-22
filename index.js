ced915cc7e28ed63d01f940cbcef772761c9cb1a
puckish-researcher-3224 <kbos22@gmail.com>
2020-03-22 14:53:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  animal = 'cat';
-  return animal;
+  function yours {
+   animal = 'cat';
+   return animal;
+  }
 }
 
 function add2(n) {
